5cef09efba371e32f3fea59e70a6570347e9c416
Liana Petrova <lianapetrova00@gmail.com>
2023-04-18 15:19:31 +0100

    fixed media query heading ordering

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,12 @@ import Search from '../Search/Search'
 const Header = () => {
   return (
     <div className='menu' >
-    <Search/>
-    <div className="header">
-        <h1>Ticket Tracker</h1>
-        <h2>By Liana Petrova</h2>
-    </div>
-    <Select/>
+      <Search/>
+      <div className="header">
+          <h1>Ticket Tracker</h1>
+          <h2>By Liana Petrova</h2>
+      </div>
+      <Select/>
     </div>
     
   )
