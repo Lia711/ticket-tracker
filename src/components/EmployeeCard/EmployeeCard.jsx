e5e97cf303cae9d621df1a262f516a8f6624f1ce
Liana Petrova <lianapetrova00@gmail.com>
2023-04-20 13:11:08 +0100

    used mapping to display employee cards

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import "./EmployeeCard.scss"
 import Counter from '../Counter/Counter'
+import team from '../../assets/data/team'
 
-const EmployeeCard = (props) => {
-  return (
+const EmployeeCard = () => {
+  return team.map((employee)=> (
     <div className="employee-card">
-      <h2 className='employeeName'>{props.employeeName}</h2>
-      <h2 className='employeeRole'>{props.employeeRole}</h2>
+      <h2 className='employeeName'>{employee.name}</h2>
+      <h2 className='employeeRole'>{employee.role}</h2>
       <Counter />
     </div>
-  )
-}
+  ))
+
+  }
 
 export default EmployeeCard
